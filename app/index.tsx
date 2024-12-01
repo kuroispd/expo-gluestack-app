e73984d7d1780f29a93a308a7fdd3b2e1feec89b
kuroispd <spdactive0@gmail.com>
2024-12-01 22:36:26 +0900

    main screen update. * Added Press Button, message, Reset Count Button.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,77 @@
+import React, { useState } from "react";
 import { Text, View } from "react-native";
+import { Box } from "@/components/ui/box";
+import { Button, ButtonText, ButtonSpinner, ButtonGroup, ButtonIcon } from "@/components/ui/button";
+import { AddIcon, CloseCircleIcon } from "@/components/ui/icon";
+import { StyleSheet } from "react-native";
+import colors, { blue, gray } from "tailwindcss/colors";
 
 export default function Index() {
+  const [isPressed, setIsPressed] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const pressed = () => {
+    setIsPressed(true);
+    setTimeout(() => {
+      setIsPressed(false);
+      setCount(count + 1);
+    }, 1000);
+  }
+
+  const resetCount = () => {
+    setCount(0);
+  }
+
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Box style={styles.main}>
+        <ButtonGroup>
+          {!isPressed ? (
+            <Button onPress={pressed} style={styles.button}>
+              <ButtonIcon as={AddIcon} className="ml-2" />
+              <ButtonText>Press me!</ButtonText>
+            </Button>
+          ) : (
+            <Button className="p-3" style={styles.button}>
+              <ButtonSpinner color={colors.gray[400]} />
+              <ButtonText className="font-medium text-sm ml-2">Please wait...</ButtonText>
+            </Button>
+          )}
+        </ButtonGroup>
+        <Text style={styles.text}>You pressed the button {count} times</Text>
+      </Box>
+      <Box style={styles.counter}>
+      {count >= 1 && (
+        <Button onPress={resetCount}>
+          <ButtonIcon as={CloseCircleIcon} className="ml-2" />
+          <ButtonText>Reset Count</ButtonText>
+        </Button>
+      )}
+      </Box>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  main : {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button : {
+  },
+  text: {
+    fontSize: 24,
+    marginTop: 20
+  },
+  counter: {
+    fontSize: 24,
+    marginTop: 20,
+  }
+});
