5470c1ab607ae72e77aa9cd5af5a42ea9090b7aa
kuroispd <spdactive0@gmail.com>
2024-12-03 20:01:23 +0900

    changed GlueStackUIProvider import path

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,7 @@
 import { Stack } from "expo-router";
 
 import "@/global.css";
-import { GluestackUIProvider } from "@/src/components/ui/gluestack-ui-provider";
+import { GluestackUIProvider } from "@components/ui/gluestack-ui-provider";
 
 export default function RootLayout() {
   return <GluestackUIProvider mode="light"><Stack /></GluestackUIProvider>;
