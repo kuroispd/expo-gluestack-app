c451f8fa827c6277e3be52b4d8b9bc0109341283
kuroispd <spdactive0@gmail.com>
2024-12-05 21:26:08 +0900

    Checking issue * ButtonIcon * Drawer (build on macOS)

--- README.md
+++ README.md
@@ -3,8 +3,6 @@
 * Windows 11 23H2
 * Node.js v20.18.1 or v22.11.0(Switching by Volta)  
 
-## What for?
-My general work is developing Laravel web application.  
-Requests of developing native application led met to start learning React Native.  
-  
-I'd like to develop this repository not only checking how to create native mobile app.
+## Issue
+`npx expo start` doesn`t build Drawer component successfully on macOS.  
+It successfully worked on Windows.
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,16 +3,28 @@ import { useNavigation } from "expo-router";
 import { Box } from "@components/ui/box";
 import { Button, ButtonText, ButtonSpinner, ButtonGroup } from "@components/ui/button";
 import { Center } from "@components/ui/center";
+import {
+  Drawer,
+  DrawerBackdrop,
+  DrawerContent,
+  DrawerHeader,
+  DrawerBody,
+  DrawerFooter,
+} from "@components/ui/drawer";
+import { Heading } from "@components/ui/heading";
 import { Text } from "@components/ui/text";
 import { Icon, AddIcon, CloseCircleIcon } from "@components/ui/icon";
 import { VStack } from "@components/ui/vstack";
 
 /**
- * issue: ButtonIcon could not use Icon components for as prop
- * 
+ * issue:
+ * * ButtonIcon could not use Icon components for as prop
+ * * Drawer doesn`t work on macOS?
  */
 export default function Index() {
   const navigation = useNavigation();
+
+  const [showDrawer, setShowDrawer] = useState(false);
 
   useEffect(() => {
     navigation.setOptions({ headerShown: false });
@@ -34,35 +46,74 @@ export default function Index() {
   }
 
   return (
-    <Box className="flex items-center justify-center h-screen">
-      <VStack space="md">
-        <Box className="w-auto h-auto">
-          <Center>
-            {!isPressed ? (
-              <Button onPress={pressed} action="positive">
-                <Icon as={AddIcon} className="text-white w-5 h-5" />
-                <ButtonText>Press me!</ButtonText>
-              </Button>
-            ) : (
-              <Button className="bg-green-900" action="positive" disabled={true}>
-                <ButtonSpinner />
-                <ButtonText className="font-medium text-sm ml-2">Please wait...</ButtonText>
-              </Button>
-            )}
-            <Text>You pressed the button {count} times</Text>
-          </Center>
-        </Box>
-        <Box className="h-[100px] w-auto">
-          <Center>
-            {count >= 1 && (
-              <Button onPress={resetCount} className="w-[200px]" action="negative">
-                <Icon as={CloseCircleIcon} className="text-white w-5 h-5" />
-                <ButtonText>Reset Count</ButtonText>
-              </Button>
-            )}
-          </Center>
-        </Box>
-      </VStack>
+    <Box className="h-full w-full">
+      <Center className="flex-1">
+        <VStack space="md">
+          <Button
+            onPress={() => {
+              setShowDrawer(true);
+            }}
+            >
+            <ButtonText>Open Drawer</ButtonText>
+          </Button>
+          <Box className="w-auto h-auto">
+            <Center>
+              {!isPressed ? (
+                <Button onPress={pressed} action="positive">
+                  <Icon as={AddIcon} className="text-white w-5 h-5" />
+                  <ButtonText>Press me!</ButtonText>
+                </Button>
+              ) : (
+                <Button className="bg-green-900" action="positive" disabled={true}>
+                  <ButtonSpinner />
+                  <ButtonText className="font-medium text-sm ml-2">Please wait...</ButtonText>
+                </Button>
+              )}
+              <Text>You pressed the button {count} times</Text>
+            </Center>
+          </Box>
+          <Box className="h-[100px] w-auto">
+            <Center>
+              {count >= 1 && (
+                <Button onPress={resetCount} className="w-[200px]" action="negative">
+                  <Icon as={CloseCircleIcon} className="text-white w-5 h-5" />
+                  <ButtonText>Reset Count</ButtonText>
+                </Button>
+              )}
+            </Center>
+          </Box>
+        </VStack>
+      </Center>
+      <Drawer
+        isOpen={showDrawer}
+        onClose={() => {
+          setShowDrawer(false)
+        }}
+        size="md"
+        anchor="left"
+      >
+        <DrawerBackdrop />
+        <DrawerContent>
+          <DrawerHeader>
+            <Heading size="3xl">Heading</Heading>
+          </DrawerHeader>
+          <DrawerBody>
+            <Text size="2xl" className="text-typography-800">
+              Drawer shows successfully?
+            </Text>
+          </DrawerBody>
+          <DrawerFooter>
+            <Button
+              onPress={() => {
+                setShowDrawer(false)
+              }}
+              className="flex-1"
+            >
+              <ButtonText>Close</ButtonText>
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
     </Box>
   );
 }
